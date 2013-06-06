7105de7028cc350d2afbd090c640bafc935b7f0e
henrietta <piotr.maslanka@henrietta.com.pl>
2013-06-07 01:15:33 +0200

    Extra unit test for RTM_MANUAL
    Handover (situation where retransmitted packet has it's content changed
    in RTM_MANUAL) unit test added

--- lnx2/unittests/channel.py
+++ lnx2/unittests/channel.py
@@ -74,3 +74,30 @@ class ChannelUnitTests(unittest.TestCase):
 
         self.assertEquals(bob_0.read(), bytearray('DUPA'))        
         self.assertEquals(bob_0.read(), None)
+
+
+    def test_RTM_MANUAL_handover(self):
+        """Tests an in-transmission packet content change"""
+        alice_0 = Channel(0, RTM_MANUAL, 0.5)
+        bob_0 = Channel(0, RTM_MANUAL, 0.5)
+
+        alice_0.write(bytearray('DUPA'))
+        pk = alice_0.on_sendable()
+        self.assertEquals(isinstance(pk, Packet), True)
+
+        sleep(0.6)
+
+        alice_0.write(bytearray('STEFAN'))
+
+        pk = alice_0.on_sendable()
+        self.assertEquals(isinstance(pk, Packet), True)
+
+        bob_0.on_received(pk)
+
+        ack = bob_0.on_sendable()
+        self.assertEquals(ack.is_ack, True)
+
+        alice_0.on_received(ack)
+
+        self.assertEquals(len(alice_0.packs_in_transit), 0)        
+        self.assertEquals(bob_0.read(), bytearray('STEFAN'))
